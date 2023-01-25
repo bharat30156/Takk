f1d6e04f9a32e4543e1589dc31667143b591f0f9
bharat30156 <bk30156@gmail.com>
2023-01-25 08:05:43 +0000

    JavaSpringFrameworkSetUp
    Created a Spring Project for Takk development

---
+++ BackEnd/TakkBackEnd/src/test/java/com/Project/Takk/com/TakkBackEnd/TakkBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Project.Takk.com.TakkBackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TakkBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
