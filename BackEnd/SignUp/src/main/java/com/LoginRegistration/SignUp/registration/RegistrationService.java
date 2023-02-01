0c8cf15be6158102ca5ae8b186c2a2efe960b5ae
bharat30156 <bk30156@gmail.com>
2023-02-01 23:35:24 +0000

    Implemented new Changes in the login registration backend

--- BackEnd/SignUp/src/main/java/com/LoginRegistration/SignUp/registration/RegistrationService.java
+++ BackEnd/SignUp/src/main/java/com/LoginRegistration/SignUp/registration/RegistrationService.java
@@ -1,5 +1,8 @@
 package com.LoginRegistration.SignUp.registration;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class RegistrationService {
     public static String register(RegistrationRequest request) {
         return "works";
