759d176d21a5bc35fd468edaf6cd189489d8a0ba
bharat30156 <bk30156@gmail.com>
2023-02-02 11:53:24 +0000

    implemented some changes in the home page

---
+++ FrontEnd/takk-Home-Page/src/components/footer-scetion.js
@@ -0,0 +1,13 @@
+import "./footer-scetion.css";
+
+const FooterScetion = () => {
+  return (
+    <div className="copyright-and-information-abou-wrapper">
+      <div className="copyright-and-information">
+        @copyright and information about the TAKK
+      </div>
+    </div>
+  );
+};
+
+export default FooterScetion;
--- FrontEnd/takk-Home-Page/src/pages/home-page-of-takk.js
+++ FrontEnd/takk-Home-Page/src/pages/home-page-of-takk.js
@@ -1,64 +1,31 @@
+import AlertBar from "../components/alert-bar";
+import NavBar from "../components/nav-bar";
+import ImageVideo from "../components/image-video";
+import CommunitySection from "../components/community-section";
+import PartnersSection from "../components/partners-section";
+import FooterScetion from "../components/footer-scetion";
+import InitiativeSection from "../components/initiative-section";
+import StorySection from "../components/story-section";
 import "./home-page-of-takk.css";
 
 const HomePageOfTakk = () => {
   return (
     <main className="home-page-of-takk" id="home-page">
-      <div className="alert-message-parent" id="alert">
-        <b className="alert-message">Alert Message</b>
-        <img className="image-42-icon" alt="" src="../image-42@2x.png" />
-      </div>
-      <nav className="takk-logo-yellow-2-parent" id="Navbar">
-        <img
-          className="takk-logo-yellow-2"
-          alt=""
-          src="../takk-logo--yellow-2@2x.png"
-        />
-        <div className="frame-wrapper">
-          <div className="frame-child" />
-        </div>
-        <div className="search-parent">
-          <div className="search">Search</div>
-          <img className="image-6-icon" alt="" src="../image-6@2x.png" />
-        </div>
-        <div className="rectangle-wrapper">
-          <button className="group-child" />
-        </div>
-        <div className="sign-up">SIGN UP</div>
-        <div className="get-fundraise-parent">
-          <div className="get-fundraise">Get Fundraise</div>
-          <div className="donate">Donate</div>
-          <div className="community">Community</div>
-        </div>
-        <div className="login">LOGIN</div>
-      </nav>
-      <section className="frame-parent" id="header">
-        <div className="support-the-elderly-people-in-parent">
-          <div className="support-the-elderly-container">
-            <p className="support-the-elderly">{`Support the Elderly people `}</p>
-            <p className="in-your-community">{`in your community `}</p>
-          </div>
-          <div className="start-a-fundraising">
-            Start a fundraising initiative now to help elderly people local to
-            you this Christmas
-          </div>
-        </div>
-        <div className="imagevideo">Image/Video</div>
-        <button className="frame-item" />
-        <div className="start-fundraising">Start Fundraising</div>
-        <img className="frame-inner" alt="" />
-      </section>
+      <AlertBar />
+      <NavBar />
+      <ImageVideo />
       <div className="home-page-of-takk-inner">
-        <div className="frame-group">
+        <div className="frame-parent">
           <div className="image-7-parent">
             <img className="image-7-icon" alt="" src="../image-7@2x.png" />
             <div className="overview-of-the-initiative-for-parent">
               <div className="overview-of-the">
                 Overview of the Initiative for the users to get an idea
               </div>
-              <img className="ellipse-icon" alt="" />
+              <img className="frame-child" alt="" />
               <div className="name-of-person">Name of Person</div>
-              <div className="rectangle-container">
-                <div className="group-item" />
+              <div className="rectangle-wrapper">
+                <div className="group-child" />
               </div>
               <div className="find-out-more">Find out more</div>
               <b className="title-of-the">Title of the Initiative</b>
@@ -70,10 +37,10 @@ const HomePageOfTakk = () => {
               <div className="overview-of-the">
                 Overview of the Initiative for the users to get an idea
               </div>
-              <img className="ellipse-icon" alt="" />
+              <img className="frame-child" alt="" />
               <div className="name-of-person">Name of Person</div>
-              <div className="rectangle-container">
-                <button className="group-inner" />
+              <div className="rectangle-wrapper">
+                <button className="group-item" />
               </div>
               <div className="find-out-more">Find out more</div>
               <b className="title-of-the">Title of the Initiative</b>
@@ -85,10 +52,10 @@ const HomePageOfTakk = () => {
               <div className="overview-of-the">
                 Overview of the Initiative for the users to get an idea
               </div>
-              <img className="ellipse-icon" alt="" />
+              <img className="frame-child" alt="" />
               <div className="name-of-person">Name of Person</div>
-              <div className="rectangle-container">
-                <div className="group-item" />
+              <div className="rectangle-wrapper">
+                <div className="group-child" />
               </div>
               <div className="find-out-more">Find out more</div>
               <b className="title-of-the">Title of the Initiative</b>
@@ -97,150 +64,11 @@ const HomePageOfTakk = () => {
           <div className="see-more">See More</div>
         </div>
       </div>
-      <section className="frame-container" id="section-3">
-        <div className="frame-div">
-          <div className="image-7-parent">
-            <img className="image-7-icon" alt="" src="../image-73@2x.png" />
-            <div className="overview-of-the-initiative-for-parent">
-              <div className="overview-of-the">
-                Overview of the Community for the users to get an idea
-              </div>
-              <div className="rectangle-container">
-                <div className="group-item" />
-              </div>
-              <div className="find-out-more">Find out more</div>
-              <b className="title-of-the3">Title of the Community</b>
-            </div>
-          </div>
-          <div className="image-7-group">
-            <img className="image-7-icon" alt="" src="../image-74@2x.png" />
-            <div className="overview-of-the-initiative-for-group">
-              <div className="overview-of-the">
-                Overview of the Community for the users to get an idea
-              </div>
-              <div className="rectangle-container">
-                <div className="group-item" />
-              </div>
-              <div className="find-out-more">Find out more</div>
-              <b className="title-of-the4">Title of the Community</b>
-            </div>
-          </div>
-          <div className="image-7-container">
-            <img className="image-7-icon" alt="" src="../image-75@2x.png" />
-            <div className="overview-of-the-initiative-for-parent">
-              <div className="overview-of-the">
-                Overview of the Community for the users to get an idea
-              </div>
-              <div className="rectangle-container">
-                <div className="group-item" />
-              </div>
-              <div className="find-out-more">Find out more</div>
-              <b className="title-of-the5">Title of the Community</b>
-            </div>
-          </div>
-          <div className="see-more">See More</div>
-        </div>
-        <div className="explore-communities-wrapper">
-          <div className="explore-communities">Explore Communities</div>
-        </div>
-      </section>
-      <section className="frame-section" id="partners">
-        <div className="image-13-parent">
-          <img className="image-13-icon" alt="" src="../image-13@2x.png" />
-          <img className="image-15-icon" alt="" src="../image-15@2x.png" />
-          <img className="image-15-icon1" alt="" src="../image-15@2x.png" />
-          <img className="image-16-icon" alt="" src="../image-15@2x.png" />
-          <img className="image-17-icon" alt="" src="../image-15@2x.png" />
-          <img className="image-15-icon2" alt="" src="../image-13@2x.png" />
-          <img className="image-14-icon" alt="" src="../image-13@2x.png" />
-          <img className="image-15-icon3" alt="" src="../image-15@2x.png" />
-        </div>
-        <div className="partners-of-takk">Partners of TAKK</div>
-      </section>
-      <section className="copyright-and-information-abou-wrapper" id="footer">
-        <div className="copyright-and-information">
-          @copyright and information about the TAKK
-        </div>
-      </section>
-      <section className="frame-parent1" id="section-1">
-        <div className="frame-parent2">
-          <div className="image-7-parent">
-            <img className="image-7-icon" alt="" src="../image-76@2x.png" />
-            <div className="overview-of-the-initiative-for-parent">
-              <div className="overview-of-the">
-                Overview of the Initiative for the users to get an idea
-              </div>
-              <img className="ellipse-icon" alt="" src="../ellipse-6.svg" />
-              <div className="name-of-person">Name of Person</div>
-              <div className="rectangle-container">
-                <div className="group-item" />
-              </div>
-              <div className="find-out-more">Find out more</div>
-              <b className="title-of-the6">Title of the Initiative</b>
-            </div>
-          </div>
-          <div className="image-7-group">
-            <img className="image-7-icon" alt="" src="../image-77@2x.png" />
-            <div className="overview-of-the-initiative-for-group">
-              <div className="overview-of-the">
-                Overview of the Initiative for the users to get an idea
-              </div>
-              <img className="ellipse-icon" alt="" src="../ellipse-6.svg" />
-              <div className="name-of-person">Name of Person</div>
-              <div className="rectangle-container">
-                <div className="group-item" />
-              </div>
-              <div className="find-out-more">Find out more</div>
-              <b className="title-of-the6">Title of the Initiative</b>
-            </div>
-          </div>
-          <div className="image-7-container">
-            <img className="image-7-icon" alt="" src="../image-78@2x.png" />
-            <div className="overview-of-the-initiative-for-parent">
-              <div className="overview-of-the">
-                Overview of the Initiative for the users to get an idea
-              </div>
-              <img className="ellipse-icon" alt="" src="../ellipse-62.svg" />
-              <div className="name-of-person">Name of Person</div>
-              <div className="rectangle-container">
-                <div className="group-item" />
-              </div>
-              <div className="find-out-more">Find out more</div>
-              <b className="title-of-the6">Title of the Initiative</b>
-            </div>
-          </div>
-          <div className="see-more2">See More</div>
-        </div>
-        <div className="explore-communities-wrapper">
-          <div className="donate-to-one">
-            Donate to one of these great Initiatives today
-          </div>
-        </div>
-      </section>
-      <section className="frame-parent3" id="scetion-2">
-        <div className="image-8-parent">
-          <img className="image-8-icon" alt="" src="../image-8@2x.png" />
-          <div className="rectangle-wrapper7">
-            <button className="rectangle-button" />
-          </div>
-          <div className="elizabeths-story">
-            <p className="support-the-elderly">Elizabeth’s Story</p>
-          </div>
-          <div className="read-more">Read More</div>
-          <img className="rectangle-icon" alt="" src="../rectangle-2.svg" />
-          <div className="donate1">Donate</div>
-          <div className="lorem-ipsum-dolor">
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cumque
-            similique eaque delectus unde commodi assumenda, rerum, et doloribus
-            nobis qui eius voluptatum vitae suscipit quos quod? Earum
-            accusantium inventore, corrupti ea iure deleniti totam quia officiis
-            suscipit possimus vel asperiores
-          </div>
-        </div>
-        <div className="story-of-the-day-wrapper">
-          <div className="story-of-the">Story of the Day</div>
-        </div>
-      </section>
+      <CommunitySection />
+      <PartnersSection />
+      <FooterScetion />
+      <InitiativeSection />
+      <StorySection />
     </main>
   );
 };
