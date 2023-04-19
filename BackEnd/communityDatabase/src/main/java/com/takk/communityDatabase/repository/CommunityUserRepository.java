3542f876f258fbc9fb209af010658b60e96bc6b3
bharat30156 <bk30156@gmail.com>
2023-04-19 10:10:39 +0100

    Deleted community Old database

--- BackEnd/communityDatabase/src/main/java/com/takk/communityDatabase/repository/CommunityUserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.takk.communityDatabase.repository;
-
-import com.takk.communityDatabase.model.CommunityUser;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface CommunityUserRepository extends JpaRepository<CommunityUser, Long> {
-}
