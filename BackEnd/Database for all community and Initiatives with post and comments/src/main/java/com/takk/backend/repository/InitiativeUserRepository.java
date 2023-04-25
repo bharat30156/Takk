cd8201b2fb116df102a85be205d69b55e389c796
bharat30156 <bk30156@gmail.com>
2023-04-25 07:18:54 +0100

    delete old code

--- BackEnd/Database for all community and Initiatives with post and comments/src/main/java/com/takk/backend/repository/InitiativeUserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.takk.backend.repository;
-
-import com.takk.backend.model.InitiativeUser;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface InitiativeUserRepository extends JpaRepository<InitiativeUser, Long> {
-}
-
