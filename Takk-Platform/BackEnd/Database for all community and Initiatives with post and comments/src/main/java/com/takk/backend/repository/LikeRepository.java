b51640a4e393415b375cc6d9f80d059fd83b9ac4
wangf988 <98456860+wangf988@users.noreply.github.com>
2023-07-13 11:00:38 +0100

    Delete Takk-Platform directory

--- Takk-Platform/BackEnd/Database for all community and Initiatives with post and comments/src/main/java/com/takk/backend/repository/LikeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.takk.backend.repository;
-
-import com.takk.backend.model.Like;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LikeRepository extends JpaRepository<Like, Long> {
-}
