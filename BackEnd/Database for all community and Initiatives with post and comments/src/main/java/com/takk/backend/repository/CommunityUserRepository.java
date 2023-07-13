99c8cd1dc5070e07eb5ff2eea1cdcbe66b93d0f5
wangf988 <98456860+wangf988@users.noreply.github.com>
2023-07-13 10:58:55 +0100

    Delete BackEnd directory

--- BackEnd/Database for all community and Initiatives with post and comments/src/main/java/com/takk/backend/repository/CommunityUserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.takk.backend.repository;
-
-
-import com.takk.backend.model.CommunityUser;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface CommunityUserRepository extends JpaRepository<CommunityUser, Long> {
-}
--- BackEnd/Database for all community and Initiatives with post and comments/src/main/java/com/takk/backend/repository/TagRepository.java
+++
@@ -1,7 +0,0 @@
-package com.takk.backend.repository;
-
-import com.takk.backend.model.Tag;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TagRepository extends JpaRepository<Tag, Long> {
-}
